da91708ea718497aecc8e133d72990d4bb968c3d
Mita Amara <mita.amara@emveep.com>
2024-09-10 10:47:00 +0700

    first commit

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Daftar Siswa</title>
+</head>
+<body>
+    <h1>Daftar Siswa</h1>
+    <ul>
+        <!-- @for ($i = 0; $i < sizeof($users['listNama']); $i++) 
+            <strong>{{ $users['listNama'][$i] }}</strong>: {{ $users['listNilai'][$i] }} <br>
+        @endfor */ -->
+    </ul>
+</body>
+</html>
